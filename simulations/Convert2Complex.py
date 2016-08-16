189190ed3e71f1e78b5cb10d6bd647698e20333b
Michael Crouse <croumb6@gmail.com>
2016-08-16 16:24:18 -0400

    working data parse to MLP model - low ACC.

--- simulations/Convert2Complex.py
+++ simulations/Convert2Complex.py
@@ -1,29 +1,75 @@
-
 import numpy as np
-
-
-fname = '/Users/kevin/Downloads/toa2.p2p'
+import glob
 
 
 def toa2Complex(toa, db, freq = 2.4e9):
     radian = toa*freq*2*np.pi
     return (10**(db/10.))*(np.cos(radian)+1j*np.sin(radian) )
 
-with open(fname) as f:
-    content = f.readlines()
+def parseTOALog(fname):
 
-# Get rid of the first two lines (header)
-content.pop(0)
-rx_num = int(content.pop(0))
+    with open(fname) as f:
+        content = f.readlines()
 
-# Parse file and convert things into complex number
-signal = np.zeros(rx_num, dtype = np.complex)
-for rx in range(rx_num):
-    paths = int(content.pop(0).split(' ')[1])
+    # Get rid of the first two lines (header)
+    content.pop(0)
+    rx_num = int(content.pop(0))
 
-    for path in range(paths):
-        pathid, toa, db = [float(x) for x in content.pop(0).split(' ')]
-        signal[rx] += toa2Complex(toa, db)
+    # Parse file and convert things into complex number
+    signal = np.zeros(rx_num, dtype = np.complex)
+    for rx in range(rx_num):
+        paths = int(content.pop(0).split()[1])
+
+        for path in range(paths):
+            pathid, toa, db = [float(x) for x in content.pop(0).split()]
+            signal[rx] += toa2Complex(toa, db)
 
 
-# signal is the output (complex number)
+    # signal is the output (complex number)
+    return signal
+
+def parseMDOALog(fname):
+    try:
+        with open(fname) as f:
+            content = f.readlines()
+    except:
+        return False
+
+    # Remove first 2 lines
+    content.pop(0)
+    content.pop(0)
+    # the mean angles is under the Phi column, which is 6th
+    angles = [float(rx_info.split()[5]) for rx_info in content]
+    return np.mean(angles)
+
+# /Users/mcrouse/Google Drive/MMWave/1800tx/AOA-Outdoor.toa.t018_06.r003.p2m
+def getTxId(path):
+    fname = path.split('/')[-1]
+    tx_info = fname.split('.')[2]
+    return int(tx_info.split('_')[0][1:])
+
+
+def loadData(base_dir, file_format):
+
+    base_dir = '/Users/mcrouse/Google Drive/MMWave/1800tx/'
+
+    file_format = 'AOA-Outdoor.%s.t%s_06.r003.p2m'
+    toa_file_list = glob.glob(base_dir + file_format % ('toa', '*'))
+    mdoa_file_list = glob.glob(base_dir + file_format % ('mdoa', '*'))
+    file_format = 'AOA-Outdoor.%s.t%03d_06.r003.p2m'
+
+    phi_off = []
+    label = []
+    for idx, fname in enumerate(toa_file_list):
+        txId = getTxId(fname)
+        phi_off.append(parseTOALog(fname))
+        angle =  parseMDOALog(base_dir + file_format % ('mdoa', txId))
+        if angle:
+            label.append(angle)
+        else:
+            phi_off.pop()
+
+    return np.array(phi_off), np.array(label)
+
+
+
---
+++ simulations/rayTraceAOA.py
@@ -0,0 +1,93 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy
+from scipy.ndimage.morphology import binary_dilation, binary_erosion
+from matplotlib import colors
+import sys
+import itertools
+from sklearn.cross_validation import LeavePOut, train_test_split
+from sklearn.svm import SVC as SVM
+from sklearn.linear_model import LogisticRegression as LR
+from sklearn.neighbors import KNeighborsClassifier as KNN
+from sklearn.neighbors import KNeighborsRegressor as KNNR
+
+from MLP import MLPClassifier
+from MLP import MLPRegressor
+import math
+from Convert2Complex import *
+
+plt.ion()
+
+
+########################################################################################
+#### Utilities
+########################################################################################
+
+def angle2c(theta):
+    return 1j*np.sin(theta)+np.cos(theta)
+def steeringVector(theta):
+    phase_diff = distance_between_rx*np.cos(theta)*2*np.pi/wavelength
+    return angle2c(np.array([rx*phase_diff for rx in range(rx_num)]))
+def angleDiff(signal1, signal2):
+    return np.mod(np.angle(signal1)-np.angle(signal2)+np.pi, 2*np.pi)-np.pi    
+def angular_diff(samples):
+    return [angleDiff( samples[i+1], samples[i] ) for i in range(len(samples)-1) ]
+def shift2degree(x):
+    return np.arccos(x)/np.pi*180.
+def degree2shift(x):
+    return (np.cos(x/180.*np.pi))
+
+def identity(x):
+    return x
+
+_colors = ['b','g','r','m','c','k']
+
+########################################################################################
+#### Classifier Initialization
+########################################################################################
+
+regressors = []
+regressors.append( KNNR(n_neighbors=3))
+regressors.append( MLPRegressor(hidden_layer_sizes=(400,50), activation='relu', verbose=False,
+    algorithm='adam', alpha=0.000, tol=1e-5, early_stopping=True))
+
+
+
+########################################################################################
+#### Load Data 
+########################################################################################
+
+#X, Y = loadData('','')
+X = np.load('X.npy')
+Y = np.load('Y.npy')
+
+#print X.shape, Y.shape
+X = np.angle(X)
+
+X = X[:,1:] - X[:,:-1]
+
+########################################################################################
+#### Test models
+########################################################################################
+
+
+# expand data (corresponds to nonlinear kernels)
+X = np.hstack([X**p for p in range(1,4)])
+
+trainX, testX, trainY, testY = train_test_split(X,Y, test_size=0.1)
+
+plt.figure(1); plt.clf();
+for i, regressor in enumerate(regressors):
+    regressor.fit(trainX, trainY)
+
+    plt.plot(testY, regressor.predict(testX) , _colors[i]+'o', alpha=0.4, label=regressor.__class__.__name__)
+    plt.plot(trainY, regressor.predict(trainX) , _colors[i]+'.', alpha=0.4)
+
+    print "{} precision = {:.4f}".format(regressor.__class__.__name__, regressor.score(testX, testY))
+
+plt.plot(testY, testY, 'k-.', alpha=1, label='ground truth')
+plt.legend(loc='best')
+
+
+
+
