Fixed bug in structured non-backprop NLP, runs with and without noise, 2D, 3D

simulations/models.py
@@ -74,17 +74,21 @@ class NBPStructuredMLP():
     def __init__(self, n_in, n_lower, n_upper, n_out):
 
         num_pairs = 2
-        ndim = n_out
+        self.ndim = n_out
         self.lower_models_l = []
-        self.lower_models_l.append(BaseMLP(n_in/num_pairs, n_lower, ndim))
-        self.lower_models_l.append(BaseMLP(n_in/num_pairs, n_lower, ndim))
+        # print "HERE: ", n_in
+        # assert False
+        self.lower_models_l.append(BaseMLP(n_in/num_pairs, n_lower, self.ndim))
+        self.lower_models_l.append(BaseMLP(n_in/num_pairs, n_lower, self.ndim))
 
-        self.upper_model = BaseMLP(n_lower[-1]*num_pairs, n_upper, ndim)
+        self.upper_model = BaseMLP(n_lower[-1]*num_pairs, n_upper, self.ndim)
+        
 
     def trainModel(self, trainXs, trainY, testXs, testY, n_epoch=200, batchsize=100, max_flag=False):
 
         output_testXs = []
         output_trainXs= []
+        print "LEN: ", len(self.lower_models_l)
         for i, model in enumerate(self.lower_models_l):
             # Setup optimizer
             optimizer = optimizers.Adam()
@@ -92,8 +96,14 @@ class NBPStructuredMLP():
 
             tmp_trainXs = []
             tmp_testXs = []
-            tmp_trainXs.append(trainXs[0][:,(i*ndim):(i*ndim)+ndim])
-            tmp_testXs.append(testXs[0][:,(i*ndim):(i*ndim)+ndim])
+            tmp_trainXs.append(trainXs[0][:,(i*self.ndim*2):(i*self.ndim*2)+self.ndim*2])
+            tmp_testXs.append(testXs[0][:,(i*self.ndim*2):(i*self.ndim*2)+self.ndim*2])
+
+            print "HERE MARCUS: ", tmp_trainXs[0].shape
+            print "HERE MARCUS: ", tmp_testXs[0].shape
+            print "HERE MARCUS: ", trainY.shape
+            print "HERE MARCUS: ", testY.shape
+            # assert False
 
             model, loss = train_model(model, tmp_trainXs, trainY,
                                             tmp_testXs, testY,
@@ -131,7 +141,7 @@ class NBPStructuredMLP():
         output_testXs = []
         for i, model in enumerate(self.lower_models_l):
             tmp_testXs = []
-            tmp_testXs.append(X[0][:,(i*ndim):(i*ndim)+ndim])
+            tmp_testXs.append(X[0][:,(i*self.ndim*2):(i*self.ndim*2)+self.ndim*2])
 
             x = chainer.Variable(np.asarray(tmp_testXs[0]))
             output_testXs.append( model.forward(x) )

simulations/plotting.py
@@ -1,5 +1,8 @@
 import matplotlib.pyplot as plt
 import numpy as np
+from mpl_toolkits.mplot3d.art3d import Poly3DCollection
+from matplotlib import cm
+from matplotlib.ticker import LinearLocator, FormatStrFormatter
 
 def plotStations(baseStations, station_len):
     for bs in baseStations:
@@ -13,7 +16,9 @@ def plotStations(baseStations, station_len):
 def plotStations3D(baseStations, station_len, fig):
     for bs in baseStations:
         cords = np.array(bs[0])
-        slope = np.tan(np.radians(bs[1]))
+        slope = np.tan(np.radians(bs[1][0]))
+        print "CORDS: ", cords
+        print "SLOPE: ", slope
         eq = to_coef(slope, cords)
         plotPlane(eq, bs[0], fig)
 
@@ -74,5 +79,87 @@ def plot_error(true_pos, predicted_pos, error, bases, title, saveflag, dir_name)
 
 
     if true_pos.shape[1] == 3:
-        print "Not implemented yet"
+        # print "BASES: ", bases
+        # assert False
+        fig = plt.figure()
 
+        ax = fig.add_subplot(111, projection='3d')
+        ax.scatter(true_pos[:,0], true_pos[:,1], true_pos[:,2], c=error)
+        ax.set_xlabel('X Plane')
+        ax.set_ylabel('Y Plane')
+        ax.set_zlabel('Z Plane')
+        # plotStations3D(bases[0], 2, fig)
+        # plotStations3D(bases, 3, fig)
+        # ax.colorbar()
+        # ax.ylim([-5,5])
+        # ax.xlim([-5,5])
+        ax.set_ylim((-6,6))
+        ax.set_xlim((-6,6))
+        ax.set_zlim((-6,6))
+        plt.title("Num Stations: %d" % (3))
+        # plt.clim([0,1])
+        # plt.show()
+        # assert False
+        if saveflag:
+            plt.savefig(dir_name + 'error_true_fig.png', format = 'png')
+        # assert False
+
+        # plt.subplot(2,1,1)
+        fig = plt.figure()
+        ax = fig.add_subplot(111, projection='3d')
+        ax.scatter(predicted_pos[:,0], predicted_pos[:,1], predicted_pos[:,2], c=error)
+        ax.set_xlabel('X Label')
+        ax.set_ylabel('Y Label')
+        ax.set_zlabel('Z Label')
+        # plotStations3D(bases, 2, fig)
+        # plotStations3D(bases, 2, fig)
+        # plt.colorbar()
+        # plt.ylim([-5,5])
+        # plt.xlim([-5,5])
+        plt.title("Num Stations: %d" % (3))
+        # plt.clim([0,1])
+        # plt.show()
+        # fig.set_size_inches(6.5, 10.5, forward=True)
+        # plt.show()
+        if saveflag:
+            plt.savefig(dir_name + 'error_predicted_fig.png', format = 'png')
+
+def plotPlane(eq, center, fig): 
+    print "EQ: ", eq
+    print "CENTER: ", center
+
+    x1 = center[0] + .2
+    x2 = center[0] - .2
+    y1 =  (eq[1] - eq[0][0]*x1)/(eq[0][1])
+    y2 =  (eq[1] - eq[0][0]*x2)/(eq[0][1])
+
+    print "x1: ", x1
+    print "x2: ", x2
+    print "y1: ", y1
+    print "y2: ", y2
+    print "*******"
+
+    # print x1
+    # print x2
+    # print y1[0]
+    # print y2[0]
+    # assert False
+    
+    # '''
+    x = [x1,x2,x2,x1]
+    # y = [y1[0],y2[0],y2[0],y1[0]]
+    y = [y1,y2,y2,y1]
+    z = [-1,-1,1,1]
+    verts = [zip(x,y,z)]
+    # '''
+    
+    '''
+    x = [1,0,0,1]
+    y = [3,0,0,3]
+    z = [0,0,1,1]
+    verts = [zip(x,y,z)]
+    '''
+
+    # fig = plt.figure()
+    ax = fig.gca(projection='3d')
+    ax.add_collection3d(Poly3DCollection(verts))