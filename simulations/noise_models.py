3221a158341e8caba2fb2710a241139adbfaeb32
marcuscomiter <marcuscomiter@gmail.com>
2016-08-30 21:15:23 -0400

    Moved dispatcher to noise_models.py and changed driver_base

--- simulations/noise_models.py
+++ simulations/noise_models.py
@@ -1,5 +1,28 @@
 import numpy as np
 
+def add_noise_dispatcher(angles, noise_model, col_idxs=range(angles.shape[1]), noise_params=None):
+    if noise_model == 'add_distribution_noise':
+        noisy_angles = add_distribution_noise(angles, col_idxs=range(angles.shape[1]),
+                                                    noise_params={'noise_type'='gaussian', 'mean': 0, 'std': 1} )
+
+    elif noise_model == 'add_angle_dependent_noise':
+        noisy_angles = add_distribution_noise(angles, col_idxs=range(angles.shape[1]),
+                                                    noise_params={'noise_type'='gaussian', 'corruption_rate': 0.1, 'lower_bound': -1, 'upper_bound':1} )
+
+    elif noise_model == 'add_spurious_noise':
+        noisy_angles = add_distribution_noise(angles, col_idxs=range(angles.shape[1]),
+                                                    noise_params={'noise_type'='gaussian', 'corruption_rate': 0.1, 'lower_bound': -1, 'upper_bound':1} )
+
+    elif noise_model == 'transform_to_nonsense_noise':
+        noisy_angles = add_distribution_noise(angles, col_idxs=range(angles.shape[1]),
+                                                    noise_params={'noise_type'='gaussian', 'lower_bound': -1, 'upper_bound':1} )
+
+    else:
+        assert False, "There is no noise model matching the one you have selected in the config file"
+
+    return noisy_angles
+
+
 # adds noise to the data from the given distribution (default is gaussian N(0,1))
 def add_distribution_noise(data, ndim,  base_idxs=[-1], noise_type="gaussian", noise_params={'mean': 0, 'std':1}):
 
@@ -28,11 +51,13 @@ def add_distribution_noise(data, ndim,  base_idxs=[-1], noise_type="gaussian", n
     return data
 
 
+# nonlinear function for add_angle_dependent_noise
 def nonlinear_effect_function(data, k, j):
     multiplier_data = np.exp(np.abs(k * (data - 90))) - j
     return multiplier_data
 
 
+# adds noise based on the angle of the point to the given base station
 def add_angle_dependent_noise(data, ndim,  base_idxs=[-1], noise_type="gaussian", noise_params={'k': 2, 'j': 1, 'mean': 0, 'std':1}):
     if noise_type == "gaussian":
         if ndim == 2:
@@ -66,10 +91,6 @@ def add_angle_dependent_noise(data, ndim,  base_idxs=[-1], noise_type="gaussian"
 
 
     return data
-
-
-
-
 
 
 def add_spurious_noise(data, ndim,  base_idxs=[-1], noise_type="uniform", noise_params={'corruption_rate': 0.1, 'lower_bound': -1, 'upper_bound':1}):
