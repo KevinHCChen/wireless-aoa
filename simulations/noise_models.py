45043bee031f0302154eb405df874ccac3a13d2c
marcuscomiter <marcuscomiter@gmail.com>
2016-08-30 20:40:05 -0400

    add_spurious_noise model added to noise_models.py

--- simulations/noise_models.py
+++ simulations/noise_models.py
@@ -33,9 +33,30 @@ def add_angle_dependent_noise():
     return
 
 
-def add_spurious_noise():
-    print "not implemented"
-    return
+def add_spurious_noise(data, ndim,  base_idxs=[-1], noise_params={'corruption_rate': 0.1, 'lower_bound': -1, 'upper_bound':1}):
+  if ndim == 2:
+      # using 1 angle for each base station (alpha)
+      col_idxs = base_idxs
+      row_idxs = np.random.permutation(data.shape[0])[:data.shape[0]*noise_params['corruption_rate']]
+      noise = np.random.uniform(low=noise_params['lower_bound'],\
+                                     high=noise_params['upper_bound'],\
+                                     size=(len(row_idxs),len(col_idxs)))
+
+      data[row_idxs,col_idxs] = noise
+  
+  elif ndim == 3:
+      # using 3 angles for each base station (alpha beta gamma)
+      for idx in base_idxs:
+          row_idxs = np.random.permutation(data.shape[0])[:data.shape[0]*noise_params['corruption_rate']]
+          noise = np.random.uniform(low=noise_params['lower_bound'],\
+                                     high=noise_params['upper_bound'],\
+                                     size=(len(row_idxs),len(col_idxs)))
+          data[:, (idx*3):(idx*3)+3] = noise
+    
+
+
+
+    return data
 
 # replaces the data with nonsense random noise from the given distribution (default is uniform [-1,1])
 def transform_to_nonsense_noise(data, ndim,  base_idxs=[-1], noise_type="uniform", noise_params={'lower_bound': -1, 'upper_bound':1}):
@@ -43,19 +64,19 @@ def transform_to_nonsense_noise(data, ndim,  base_idxs=[-1], noise_type="uniform
       if ndim == 2:
           # using 1 angle for each base station (alpha)
           col_idxs = base_idxs
-          gauss_noise = np.random.uniform(low=noise_params['lower_bound'],\
+          noise = np.random.uniform(low=noise_params['lower_bound'],\
                                      high=noise_params['upper_bound'],\
                                      size=(data.shape[0],len(col_idxs)))
 
-          data[:,col_idxs] += gauss_noise
+          data[:,col_idxs] += noise
       
       elif ndim == 3:
           # using 3 angles for each base station (alpha beta gamma)
           for idx in base_idxs:
-              gauss_noise = np.random.uniform(lower=noise_params['lower_bound'],\
+              noise = np.random.uniform(lower=noise_params['lower_bound'],\
                                      high=noise_params['upper_bound'],\
                                      size=(data.shape[0],ndim))
-              data[:, (idx*3):(idx*3)+3] += gauss_noise
+              data[:, (idx*3):(idx*3)+3] += noise
     
     else:
         assert False, "This type of transform to nonsense noise has not been implemented in noise_models.py"
