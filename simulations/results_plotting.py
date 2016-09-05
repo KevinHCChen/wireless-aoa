2c6b0580668618eef668ff8711a4d7ea3220986b
Michael Crouse <croumb6@gmail.com>
2016-09-05 17:14:52 -0400

    updated results_plotting for correct aggregated plotting

--- simulations/results_plotting.py
+++ simulations/results_plotting.py
@@ -1,17 +1,18 @@
 import numpy as np
+import glob
 import pandas as pd
 import plotly as py
 import plotly.graph_objs as go
 import argparse
 
 def plot_increased_training(data, out_file):
-    nn_sizes = data['NN__network_size'].unique()
-    net_types = data['NN__type'].unique()
+    nn_sizes = np.sort(data['NN__network_size'].unique())
+    net_types = np.sort(data['NN__type'].unique())
 
     data_list = []
     for n_type in net_types:
         select = data[data['NN__type'] == n_type]
-        nn_sizes = select['NN__network_size'].unique()
+        nn_sizes = np.sort(select['NN__network_size'].unique())
         for nn_size in nn_sizes:
             crit_str = nn_size.replace('[', '\[').replace(']', '\]')
             select2 = select[select['NN__network_size'].str.match(crit_str)]
@@ -23,12 +24,12 @@ def plot_increased_training(data, out_file):
                 data_list.append(go.Scatter(
                     x = x,
                     y = y,
-                    name='%s-%s' %(n_type, nn_size)
                     #error_y=dict(
                     #            type='data',
-                    #            array=np.sqrt(select2['std_err']),
+                    #            array=select2['std_err'],
                     #            visible=True
-                    #        )
+                    #        ),
+                    name='%s-%s' %(n_type, nn_size)
                 ))
 
 
@@ -40,17 +41,23 @@ def plot_increased_training(data, out_file):
 
     py.offline.plot(fig, filename=out_file)
 
+def load_results(res_dir):
+    fns = glob.glob(res_dir+"/*.csv")
+    data = [pd.read_csv(fn) for fn in fns]
+    return data
+
 
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(description='Plotting for 5G Experiments')
-    parser.add_argument('--results_file', '-f', dest='config_file', type=str,
+    parser.add_argument('--results_dir', '-d', dest='results_dir', type=str,
                         help='Which config file to use')
 
     args = parser.parse_args()
 
-    data = pd.read_csv(args.config_file)
-    exp_name = args.config_file.split("/")[1].split(".")[0]
+    data_l = load_results(args.results_dir)
+    data = pd.concat(data_l, ignore_index=True)
     base_dir = "summary_plots/"
-
+    exp_name = "%s" % (args.results_dir)
     plot_increased_training(data, base_dir + exp_name + "_increasing_training.html")
+
