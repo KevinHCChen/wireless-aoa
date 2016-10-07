799760e0a37ea4abf5486468e3b9904716993b84
Michael <croumb6@gmail.com>
2016-10-07 10:46:12 -0400

    several additions to start_exp to handle more antennas generally

--- scripts/start_exp.sh
+++ scripts/start_exp.sh
@@ -9,7 +9,7 @@ DELL_RX=192.168.12.61
 REF=/root/aoa/ones1000.dat
 FREQ=916000000
 RATE=500000
-RX=4
+RX=$2
 BURST=2
 
 # sudo ifconfig en5 192.168.12.77 up
@@ -27,13 +27,26 @@ ssh root@$DELL_TX "sysctl -w net.core.rmem_max=50000000"
 
 
 echo "Starting RX - Run $1"
-ssh root@$DELL_RX "sh -c 'nohup  /root/aoa/4rx_to_file \\
---freq=$FREQ --rate=$RATE --gain=25 --run=$1 --runtime 4 \\
---ip1=addr=192.168.11.14 --ip2=addr=192.168.11.15  --ip3=addr=192.168.11.16 --ip4=addr=192.168.11.18 \\
-> /dev/null 2>&1 &'"
+#ssh root@$DELL_RX "sh -c 'nohup  /root/aoa/4rx_to_file \\
+if [ $RX -eq 4 ]; then
+  ssh root@$DELL_RX "sh -c 'nohup  /root/aoa/GEN_rx_to_file --freq=$FREQ --rate=$RATE --gain=25 --run=$1 --runtime 4 --ip1=addr=192.168.11.15  --ip2=addr=192.168.11.16  --ip3=addr=192.168.11.18  --ip4=addr=192.168.11.20 > /dev/null 2>&1 &'"
+  sleep 2.5
+elif [ $RX -eq 3 ]; then
+  ssh root@$DELL_RX "sh -c 'nohup  /root/aoa/GEN_rx_to_file --freq=$FREQ --rate=$RATE --gain=25 --run=$1 --runtime 8 --ip1=addr=192.168.11.20  --ip2=addr=192.168.11.21 --ip3=addr=192.168.11.22 > /dev/null 2>&1 &'"
+  sleep 2.5
+elif [ $RX -eq 5 ]; then
+  ssh root@$DELL_RX "sh -c 'nohup  /root/aoa/GEN_rx_to_file --freq=$FREQ --rate=$RATE --gain=25 --run=$1 --runtime 8 --ip1=addr=192.168.11.14 --ip2=addr=192.168.11.15  --ip3=addr=192.168.11.16  --ip4=addr=192.168.11.18 --ip5=addr=192.168.11.20 > /dev/null 2>&1 &'"
+  sleep 3.5
+elif [ $RX -eq 7 ]; then
+  echo "Running 7 rx nodes!"
+  ssh root@$DELL_RX "sh -c 'nohup  /root/aoa/GEN_rx_to_file --freq=$FREQ --rate=$RATE --gain=25 --run=$1 --runtime 8 --ip1=addr=192.168.11.14 --ip2=addr=192.168.11.15  --ip3=addr=192.168.11.16  --ip4=addr=192.168.11.18  --ip5=addr=192.168.11.20 --ip6=addr=192.168.11.21  --ip7=addr=192.168.11.22 > /dev/null 2>&1 &'"
+  sleep 4.5
+else
+  echo "Bad # Rxs - reset param"
+  exit
+fi
 
-sleep 2.5
-# sleep 4.5
+
 echo "Start TX"
 ssh root@$DELL_TX "/root/aoa/start_tx.sh $FREQ $RATE $REF $REF" 
 sleep 3
@@ -47,5 +60,5 @@ echo "Pulling data"
 scp root@$DELL_RX:/root/aoa/data/t*_$1.dat ../data/testdata/
 echo "All done!"
 
-python AOA.py --run=$1 --rx=$RX
+python AOA.py --run=$1 --rx=$RX --dRxTx=.91
 echo "python AOA.py -- --run=$1 --rx=$RX"
